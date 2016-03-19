a00facc96c0017f3e4374063cab5625cbdec0a1d
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-03-19 17:32:18 +0530

    Camera

---
+++ app/src/main/java/com/xkcd/xkcd/CameraDemoActivity.java
@@ -0,0 +1,60 @@
+package com.xkcd.xkcd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CameraDemoActivity extends Activity {
+    int TAKE_PHOTO_CODE = 0;
+    public static int count = 0;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+
+        // Here, we are making a folder named picFolder to store
+        // pics taken by the camera using this application.
+        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
+        File newdir = new File(dir);
+        newdir.mkdirs();
+        count++;
+
+        String file = dir+count+".jpg";
+        File newfile = new File(file);
+        try {
+            newfile.createNewFile();
+        }
+        catch (IOException e)
+        {
+        }
+
+        Uri outputFileUri = Uri.fromFile(newfile);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+            Log.d("CameraDemo", "Pic saved");
+        }
+    }
+}
